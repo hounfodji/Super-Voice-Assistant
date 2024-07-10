1c3aae1e73841309b67fb4f00bfa50a0244b5a5d
hounfodji <hospicehounfodji@gmail.com>
2024-07-10 12:34:31 +0100

    Delete audio_gemini app

--- audio_gemini/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('record/', views.record_audio, name='record_audio'),
-    path('process/', views.process_audio, name='process_audio'),
-]
